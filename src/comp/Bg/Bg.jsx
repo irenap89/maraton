c269bf0198bc00b3cd14ac1babd298851028bb37
irenap89 <irenap89@gmail.com>
2023-06-19 19:15:19 +0300

    css changes

--- src/comp/Bg/Bg.jsx
+++ src/comp/Bg/Bg.jsx
@@ -2,18 +2,59 @@
 import './Bg.css';
 
 import close from './../../images/close.png'
+import banner from './../../images/banner.png'
+import logo from './../../images/logo.png'
 
 function Bg() {
   return (
    <div className="bg_general_div">
         <div className='bg_top_menu'> 
             <img src={close} className='bg_close_btn'/>
-            <div className='bg_top_menu_title' > העלאת תמונה כדי להסיר את הרקע </div>
+            <div className='bg_top_menu_title'> העלאת תמונה כדי להסיר את הרקע </div>
             <div className="bg_top_menu_button"> העלאת תמונה </div>
             <div className="bg_top_menu_sub_title"> פורמטים נתמכים: png, jpeg</div>
 
         </div>
 
+
+        <div className='bg_body_area'>
+          <div className='bg_body_left'> 
+            <div className='bg_body_tabs'>
+              <div className="bg_body_bg_remove"> 
+              <span className="bg_body_bg_remove_text">הוסר רקע  </span>
+              
+              </div>
+              <div className="bg_body_bg_original">
+              <span className="bg_body_bg_remove_text">
+                מקורי 
+                </span>
+                </div>
+            </div>
+
+            <div className="bg_body_left_middle">
+
+            </div>
+              <div className="bg_body_eula">
+                על ידי העלאת תמונה אתה מסכים לתנאים וההגבלות שלנו. וחלים מדיניות ופרטיות ותנאי השימוש
+              </div>
+          </div>
+
+
+
+          <div className='bg_body_right'> 
+            <div className="bg_body_right_middle">
+
+            </div>
+          </div>
+
+        </div>
+
+
+        <div className='bg_footer'> 
+          <img src={banner} className='bg_footer_banner'/>
+          <img src={logo} className='bg_footer_logo'/>
+         
+        </div>
 
    </div>
   );
